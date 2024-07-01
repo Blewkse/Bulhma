468fbb6367cb8533c3da7d54f2abe08745c9cab4
Renaud-HUSSON <renaudhusson09@gmail.com>
2024-07-01 14:05:22 +0200

    ci: launch e2e and perf tests with docker compose

---
+++ start_e2e_test.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose.test.e2e.yml run frontend
---
+++ start_perf_test.sh
@@ -0,0 +1,2 @@
+docker compose -f docker-compose.test.perf.yml start nginx
+docker compose -f docker-compose.test.perf.yml run backend-perf-tests
